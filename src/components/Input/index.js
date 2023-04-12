19df6d02af3845fc9d4fa431b00bacd358743306
Trip25 <t.gurung1995@gmail.com>
2023-04-12 15:30:31 +0100

    done 1.4

---
+++ src/components/Input/index.js
@@ -0,0 +1,15 @@
+/*import React into file
+-Create a new functional component called Input that will return JSX (we'll come back to the specifics of this in the next part).
+-Export that function as a default export.
+-In that Input component's function, return an <input type="text" onChange={()=>""}> element in JSX; you'll use this later to take in some text.
+*/
+
+import React from 'react';
+
+function Input() {
+    return (
+    <input type="text" onChange={()=>""} />
+    )
+}
+
+export default Input;
